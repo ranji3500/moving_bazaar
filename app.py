a13870e5a2ff5f27ee5d8f377a5e4ca33071828a
Ranjith Kumar A <108408767+ranji3500@users.noreply.github.com>
2025-10-01 12:20:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -331,4 +331,4 @@ def unhandled(e):
 
 # --- Entrypoint ---------------------------------------------------------------
 if __name__ == "__main__":
-    app.run(host="localhost", port=5008, threaded=True, debug=True)
+    app.run(host="0.0.0.0", port=5008, threaded=True, debug=True)
